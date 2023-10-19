3cd4f0c219840154fbf6dcfcd75509d027427899
Digbijaya Lenka <digbijayalenka@gmail.com>
2023-10-19 17:55:44 +0530

    .

--- Array/sorting.js
+++ Array/sorting.js
@@ -1,5 +1,5 @@
 //sorting using nested for loops
-const arr = [20, 40, 30, 12, 25];
+const arr = [20, 40, 30, 12, 25,155];
 for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
         if (arr[j] > arr[j + 1]) {
--- practice.js
+++ practice.js
@@ -1,19 +1,6 @@
-let arr = [1, 2, 13, 4, 5, 6, 7, 8, 9, 10, 124];
-let find = 6;
-let start = 0;
-let end = arr.length - 1;
-let position = undefined;
-
-while (start <= end) {
-    let mid = Math.floor((start + end) / 2);
-    if (arr[mid] === find) {
-        position = mid;
-        break;
-    } else if (arr[mid] > find) {
-        end = mid - 1;
-    } else {
-        start = mid + 1;
-    }
-}
-
-console.log(position);
+let arr= ["dig", "Seecon", "Bablu"]
+// arr.forEach(function(x){
+//     console.log(x);
+// })
+let index1 = arr.indexOf("Seecon");
+console.log(index1);
